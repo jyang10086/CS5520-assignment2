a3c8c9e819a521aa7814fb3e253ef402b5f9b019
jyang10086 <janewyang10086@gmail.com>
2024-09-30 16:07:38 -0700

    add cancel and save functionality to go back and update style on AddActivity screen

--- screens/AddActivity.js
+++ screens/AddActivity.js
@@ -1,4 +1,4 @@
-import { Button, StyleSheet, Text, TextInput, View } from "react-native";
+import { Alert, Button, StyleSheet, Text, TextInput, View } from "react-native";
 import { useState } from "react";
 import DateTimePicker from "@react-native-community/datetimepicker";
 import { SelectList } from "react-native-dropdown-select-list";
@@ -34,54 +34,96 @@ export default function AddActivity({ navigation }) {
     setShowDatePicker(!showDatePicker);
   };
 
+  const handleCancel = () => {
+    setActivity("");
+    setDuration("");
+    navigation.goBack();
+  };
+
+  const validateDurationInput = (duration) => {
+    const isNumber = /^[0-9]+$/.test(duration);
+    if (!isNumber || duration.length === 0 || parseInt(duration) < 0) {
+      return false;
+    }
+    return true;
+  };
+
+  const handleSave = () => {
+    const isDurationValid = validateDurationInput(duration);
+    if (!isDurationValid || activity.length === 0 || !date) {
+      Alert.alert("Invalid input", "Please check your input values");
+      return;
+    }
+    const newActivity = {
+      id: Math.random().toString(),
+      type: activity,
+      duration: parseInt(duration),
+      date: date.toDateString(),
+    };
+
+    // todo: addActivity();
+
+    setActivity("");
+    setDuration("");
+    setDateTxt("");
+    setDate(null);
+
+    navigation.goBack();
+  };
+
   return (
     <View style={styles.container}>
       <View>
-        <View>
-          <Text style={styles.text}>Activity *</Text>
-          <SelectList
-            setSelected={handleActivityChange}
-            data={activityData}
-            save="value"
-            placeholder="Select An Activity"
-            search={false}
+        <Text style={styles.text}>Activity *</Text>
+        <SelectList
+          setSelected={handleActivityChange}
+          data={activityData}
+          save="value"
+          placeholder="Select An Activity"
+          search={false}
+        />
+      </View>
+      <View>
+        <Text style={styles.text}>Duration (min) *</Text>
+        <TextInput
+          style={styles.inputContainer}
+          value={duration}
+          onChangeText={handleDurationChange}
+          keyboardType="numeric"
+        />
+      </View>
+      <View>
+        <Text style={styles.text}>Date *</Text>
+        <TextInput
+          style={styles.inputContainer}
+          onPressIn={() => setShowDatePicker(!showDatePicker)}
+          value={dateTxt}
+          editable={false}
+        />
+        {showDatePicker && (
+          <DateTimePicker
+            testID="dateTimePicker"
+            value={date}
+            mode="date"
+            display="inline"
+            onChange={handleDateChange}
           />
-        </View>
-        <View>
-          <Text style={styles.text}>Duration (min) *</Text>
-          <TextInput
-            value={duration}
-            onChangeText={handleDurationChange}
-            keyboardType="numeric"
-          />
-        </View>
-        <View>
-          <Text style={styles.text}>Date *</Text>
-          <TextInput
-            onPressIn={() => setShowDatePicker(!showDatePicker)}
-            value={dateTxt}
-            editable={false}
-          />
-          {showDatePicker && (
-            <DateTimePicker
-              testID="dateTimePicker"
-              value={date}
-              mode="date"
-              display="inline"
-              onChange={handleDateChange}
-            />
-          )}
-        </View>
-        <View style={styles.buttonContainer}>
-          <Button title="Cancel" />
-          <Button title="Save" />
-        </View>
+        )}
+      </View>
+      <View style={styles.buttonContainer}>
+        <Button title="Cancel" onPress={handleCancel} />
+        <Button title="Save" onPress={handleSave} />
       </View>
     </View>
   );
 }
 const styles = StyleSheet.create({
-  container: {},
+  container: {
+    flex: 1,
+    padding: 10,
+    marginTop: 20,
+    rowGap: 40,
+  },
   text: {
     color: "darkslateblue",
     fontWeight: "bold",
@@ -90,5 +132,15 @@ const styles = StyleSheet.create({
   buttonContainer: {
     flexDirection: "row",
     justifyContent: "space-around",
+  },
+  inputContainer: {
+    padding: 10,
+    borderWidth: 2,
+    borderColor: "darkslateblue",
+    borderRadius: 5,
+    height: 40,
+    backgroundColor: "white",
+    color: "darkslateblue",
+    fontSize: 18,
   },
 });
