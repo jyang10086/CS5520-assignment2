97eb58bf92f5ba533fa7d6437afdd3f96da5abdf
jyang10086 <janewyang10086@gmail.com>
2024-09-30 14:46:41 -0700

    Set up navigation structure with bottom tabs inside stack navigator, initi three main screen components

--- App.js
+++ App.js
@@ -1,20 +1,38 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet} from "react-native";
+import { NavigationContainer } from "@react-navigation/native";
+import { createStackNavigator } from "@react-navigation/stack";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import Activities from "./screens/Activities";
+import Diets from "./screens/Diets";
+import Settings from "./screens/Settings";
+const Stack = createStackNavigator();
+const Tab = createBottomTabNavigator();
+function Home() {
+  return (
+    <Tab.Navigator>
+      <Tab.Screen name="Activities" component={Activities} />
+      <Tab.Screen name="Diets" component={Diets} />
+      <Tab.Screen name="Settings" component={Settings} />
+    </Tab.Navigator>
+  );
+}
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen
+          name="Home"
+          component={Home}
+          options={{ headerShown: false }}
+        />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
   },
 });
---
+++ screens/Activities.js
@@ -0,0 +1,8 @@
+import { StyleSheet, Text, View } from "react-native";
+export default function Activities() {
+  return (
+    <View>
+      <Text>activities</Text>
+    </View>
+  );
+}
